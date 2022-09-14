5613304117941fe61fbbfb5efb764a5f0bb6184e
maciej-siola <maciejsiola@gmail.com>
2022-09-14 23:54:42 +0200

    Export upload files from first wordpress instace

--- wordpress1_copy_script.sh
+++ wordpress1_copy_script.sh
@@ -2,3 +2,5 @@
 
 wp db export --tables=$(wp db tables --format=csv) /tmp/exported-wp-sql.sql
 sed --in-place --expression 's#`wp1_#`wp2_#g' /tmp/exported-wp-sql.sql
+mkdir /tmp/copy/
+cp -R /var/www/html/wp-content/uploads/* /tmp/copy/
