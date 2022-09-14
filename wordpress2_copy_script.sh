a5914f196513abd5e0b705ccb14ae7cffbb12b42
maciej-siola <maciejsiola@gmail.com>
2022-09-14 23:55:28 +0200

    Import upload content in second wordpress instance

--- wordpress2_copy_script.sh
+++ wordpress2_copy_script.sh
@@ -3,3 +3,5 @@
 wp --quiet db import /tmp/exported-wp-sql.sql
 wp search-replace "http://strona1.com" "http://strona2.com"
 rm /tmp/exported-wp-sql.sql
+cp -R /tmp/copy/* /var/www/html/wp-content/uploads/
+rm -rf /tmp/copy/
